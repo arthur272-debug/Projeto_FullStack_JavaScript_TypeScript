53924b04d092a871a130438c9380e68f7bfe695e
Arthur André <arthurdasilvaandre@gmail.com>
2024-08-17 13:49:45 -0300

    revisando_comentarios

---
+++ Aulas_Basico/aula03.js
@@ -0,0 +1,11 @@
+// revisando comentários
+
+// serve para escrever algo longo
+console.log('Tuturito chegou na área!!!');
+console.log('Linha 1'); //linha que faz algo em um programa imaginário
+console.log('Linha 2');
+/*
+server para trechos de mais uma linha
+console.log('Linha 3');
+console.log('Linha 4');
+*/
