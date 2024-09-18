a6b1f27609236c8a14d7c377b5ee08190e40c729
Arthur André <arthurdasilvaandre@gmail.com>
2024-09-18 18:47:17 -0300

    revisando_const

---
+++ Aulas_Basico/aula06/index.js
@@ -0,0 +1,9 @@
+// revisando const
+
+const primeiroNumero = 8;
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+const resultadoDuplicado = resultado * 2;
+
+console.log("Resultado:", resultado);
+console.log("Resultado duplicado:", resultadoDuplicado);
