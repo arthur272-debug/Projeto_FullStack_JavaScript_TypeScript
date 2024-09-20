cbdfe919ff0900e68c03496de70f8426013a3a56
Arthur André <arthurdasilvaandre@gmail.com>
2024-09-20 20:33:34 -0300

    diferenca_entre_var_e_let

---
+++ Aulas_Basico/aula07/index.js
@@ -0,0 +1,7 @@
+// primeira diferença entre var e let
+var nome = 'Tutuzão'; // 'var' permite a redeclaração
+var nome = 'Tutu'; // está em desuso
+let sobrenome = 'Silvinha';
+
+console.log(nome);
+console.log(sobrenome);
