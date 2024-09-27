12031283f29dfba6a9874cfbb9b83e75fbc141de
Arthur André <arthurdasilvaandre@gmail.com>
2024-09-27 19:18:09 -0300

    revisando_tipos_primitivos

---
+++ Aulas_Basico/aula08/index.js
@@ -0,0 +1,21 @@
+// tipos primitivos de dados
+
+const nome = 'Michael';
+const nome2 = 'Tuturito';
+const nome3 = 'Lipe';
+const num1 = 10;
+const num2 = 10.43;
+
+let nomeProfessor;
+const apelido = null;
+const aprovado = false;
+
+let a = 2;
+let b = a;
+
+console.log(typeof nome, nome);
+console.log(typeof nomeProfessor, nomeProfessor);
+console.log(a, b);
+
+a = 3;
+console.log(a, b);
