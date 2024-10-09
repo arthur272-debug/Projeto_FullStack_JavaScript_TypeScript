096d33d0e8f93043d3e45caa52a4e630eb31c5f3
Arthur André <arthurdasilvaandre@gmail.com>
2024-10-08 21:44:56 -0300

    exercitando_window_e_alert

--- Aulas_Basico/aula10/js/index.js
+++ Aulas_Basico/aula10/js/index.js
@@ -4,8 +4,6 @@ alert('Erro: Você vai aprender muitooo ao longo da vida!!');
 window.confirm('Deseja continuar?');
 let idade = window.prompt('Digite a sua idade:');
 let confirmacao = window.confirm('Deseja excluir o banco de dados?');
-console.log('Resposta da confirmação:', confirmacao);
-console.log('Resposta da confirmação:', idade);
-window.alert('Resposta da confirmação:', confirmacao)
-window.alert('Resposta da confirmação de idade:', idade)
+alert('Resposta da confirmação:' + confirmacao);
+alert('Resposta da confirmação de idade:' + idade);
 
---
+++ Exercicios_Basico/exercicio03/js/index.js
@@ -0,0 +1,7 @@
+const num1 = prompt('Digite um número:');
+const num2 = prompt('Digite um outro número:');
+
+const resultado = Number(num1) + Number(num2);
+
+alert('Resultado:' + resultado);
+//console.log('Resultado:' + resultado);
