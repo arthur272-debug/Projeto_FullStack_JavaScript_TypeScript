b5ffa6c3844b573d5d92f5bdcc6b2032b54c1de4
Arthur André <arthurdasilvaandre@gmail.com>
2024-10-29 21:21:22 -0300

    vendo_numbers

---
+++ Aulas_Basico/aula12/index.js
@@ -0,0 +1,21 @@
+// Mais sobre numbers -- ver e anotar no caderno
+
+let num1 = 10; // number
+let num2 = 2.5; // number
+let num3 = 10.675543487; // number
+let num4 = 0.7; // number
+let num5 = 0.1; // number
+
+// console.log(num1.toString() + num2);
+// console.log(Number.isInteger(num1));
+// num1 = num1.toString();
+// console.log(num1.toString(2)); // conversão para binário
+// console.log(num3.toFixed(3));
+// console.log(Number.isInteger(num1));
+// let temporario = num1 * 'Olá';
+// console.log(Number.isNaN(temporario));
+
+num4 += num5;
+console.log(num4);
+soma = parseFloat(num4.toFixed(2));
+console.log(soma);
