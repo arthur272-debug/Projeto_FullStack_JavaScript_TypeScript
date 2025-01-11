3f1cabc97b81b5a710e7b9fd1e76db02bcf9eb8e
Arthur André <arthurdasilvaandre@gmail.com>
2025-01-11 18:29:32 -0300

    praticando_Math

--- Exercicios_Basico/exercicio06/script.js
+++ Exercicios_Basico/exercicio06/script.js
@@ -1,3 +1,8 @@
-let numero = prompt("Digite um número: ");
+const numero = Number(prompt("Digite um número: "));
+const numeroTitulo = document.getElementById("numero-titulo");
+const texto = document.getElementById("texto");
+const texto2 = document.getElementById("texto2");
 
-
+numeroTitulo.innerHTML = numero;
+texto.innerHTML = `<p>Seu número - 2 é ${numero - 2}</p>`;
+texto2.innerHTML = `<p>Seu número + 2 é ${numero + 2}</p>`;
