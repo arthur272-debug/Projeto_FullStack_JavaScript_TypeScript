e02505fe643ed716e9c77592bcb57f1cc658d24d
Arthur André <arthurdasilvaandre@gmail.com>
2025-01-20 19:12:08 -0300

    praticando_arrays

--- Aulas_Basico/aula14/script.js
+++ Aulas_Basico/aula14/script.js
@@ -1,1 +1,22 @@
-// Revisando sobre Arrays
+// Revisando e praticando sobre Arrays
+
+const coisas = [1, null, "Tutu", true, 3.1, "Tuturitutu"];
+console.log(coisas);
+
+const alunos = ["Luizão", "Maria Bela", "Alessandro"];
+console.log(alunos);
+alunos[1] = "Mariazinha";
+console.log(alunos);
+console.log(alunos.length);
+alunos[alunos.length] = "Tchuca";
+alunos.push("Tchucão");
+console.log(alunos);
+alunos.unshift("Tuturito");
+const removido = alunos.pop();
+console.log(removido);
+console.log(alunos);
+delete alunos[0];
+console.log(alunos);
+console.log(alunos.slice(0, 1));
+console.log(alunos instanceof Array);
+console.log(typeof alunos);
