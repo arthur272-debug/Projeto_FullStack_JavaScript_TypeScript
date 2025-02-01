54aad3800832d14aa391bb506c5dedfdc9ba26e1
Arthur André <arthurdasilvaandre@gmail.com>
2025-02-01 11:35:30 -0300

    praticando_mais_ainda

--- Aulas_Basico/aula15/script.js
+++ Aulas_Basico/aula15/script.js
@@ -16,11 +16,33 @@
 // const saudacao = DarSaudacao("Tuturito");
 // console.log(saudacao);
 
-function somarValores(valor1, valor2) {
-  const resultado = valor1 + valor2;
-  return resultado;
-}
+// function somarValores(valor1, valor2) { // forma 'tradicional' de declarar uma função
+//   const resultado = valor1 + valor2;
+//   return resultado;
+// }
 
-const somatorio = somarValores(10, 20);
-console.log(somatorio);
-console.log(somarValores(100, 1000));
+// const somatorio = somarValores(10, 20); // função sem valor padrão
+// console.log(somatorio);
+// console.log(somarValores(100, 1000));
+
+// function multiplicarValores(valor1 = 0, valor2 = 0) { // função com valor padrão
+//   // forma 'moderna' de declarar uma função
+//   const resultado = valor1 * valor2;
+//   return resultado;
+// }
+
+// const multiplicacao = multiplicarValores(10, 20);
+// console.log(multiplicacao);
+// console.log(multiplicarValores());
+
+// const raiz = function (n) { // função anônima
+//   return n ** 0.5;
+// };
+
+// arrow function -0 mais moderna
+const raiz = (n) => {
+  return n ** 0.5;
+};
+
+console.log(raiz(9));
+console.log(raiz(25));
