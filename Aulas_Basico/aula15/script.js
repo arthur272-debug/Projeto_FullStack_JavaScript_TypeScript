c29932c6cdcbacd49408313609b5b95b8857a57d
Arthur André <arthurdasilvaandre@gmail.com>
2025-02-01 11:15:21 -0300

    praticando_valores

--- Aulas_Basico/aula15/script.js
+++ Aulas_Basico/aula15/script.js
@@ -1,1 +1,26 @@
 // Revisão funções
+
+// uma maneira de declarar uma função
+// function DarSaudacao(nome) {
+//   console.log(`Olá, ${nome}!`);
+// }
+
+// saudacao("Tuturito");
+// saudacao("Jessicão");
+
+// outra maneira de declarar uma função
+// function DarSaudacao(nome) {
+//   return `Olá, ${nome}!`;
+// }
+
+// const saudacao = DarSaudacao("Tuturito");
+// console.log(saudacao);
+
+function somarValores(valor1, valor2) {
+  const resultado = valor1 + valor2;
+  return resultado;
+}
+
+const somatorio = somarValores(10, 20);
+console.log(somatorio);
+console.log(somarValores(100, 1000));
