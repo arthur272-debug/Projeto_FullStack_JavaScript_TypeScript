f5f89c49633eef72f6f926df150d20df30ed0f45
Arthur André <arthurdasilvaandre@gmail.com>
2025-02-07 21:50:50 -0300

    praticando_mais_sobre_objetos

--- Aulas_Basico/aula16/scripit.js
+++ Aulas_Basico/aula16/scripit.js
@@ -10,16 +10,35 @@
 // console.log(`Idade: ${pessoal.idade}`);
 // console.log(pessoal);
 
-function criarPessoa(nome, sobrenome, idade) {
-  return {
-    nome: nome,
-    sobrenome: sobrenome,
-    idade: idade,
-  };
-}
+// uma outra forma de criar um objeto
+// function criarPessoa(nome, sobrenome, idade) {
+//   return { nome: nome, sobrenome: sobrenome, idade: idade };
+// }
 
-pessoa1 = criarPessoa("Tuturito", "Silvana", 65);
-pessoa2 = criarPessoa("Joãozinho", "Ribeiro", 98);
-pessoa3 = criarPessoa("Passarinho", "Sodré", 23);
+// pessoa1 = criarPessoa("Tuturito", "Silvana", 65);
+// pessoa2 = criarPessoa("Joãozinho", "Ribeiro", 98);
+// pessoa3 = criarPessoa("Louisa", "Young", 23);
+// pessoa4 = criarPessoa("Blake", "Campbell", 28);
+// pessoa5 = criarPessoa("Rachel Brady", "Casey", 46);
 
-console.log(pessoa1.nome, pessoa2.nome);
+// console.log(pessoa1.nome, pessoa1.sobrenome, pessoa1.idade);
+
+const pessoa_1 = {
+  nome: "Tuturito",
+  sobrenome: "Silvana",
+  idade: 25,
+
+  fazerDescricao() {
+    return console.log(
+      `Meu nome é ${this.nome} ${this.sobrenome}, tenho ${this.idade} anos.`
+    );
+  },
+
+  aumentarIdade() {
+    this.idade++;
+  },
+};
+
+pessoa_1.fazerDescricao();
+pessoa_1.aumentarIdade();
+pessoa_1.fazerDescricao();
