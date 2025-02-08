46d532497ecc034bf9394590f1f892b70daeca0e
Arthur André <arthurdasilvaandre@gmail.com>
2025-02-07 21:13:39 -0300

    praticando_sobre_objetos

--- Aulas_Basico/aula16/scripit.js
+++ Aulas_Basico/aula16/scripit.js
@@ -1,1 +1,25 @@
-// aprendendo sobre objetos - básico
+// revisando sobre objetos - básico
+
+// uma das formas de criar um objeto
+// const pessoal = {
+//   nome: "Tuturito",
+//   sobrenome: "Silvana",
+//   idade: 65,
+// };
+// console.log(`Nome: ${pessoal.nome}`);
+// console.log(`Idade: ${pessoal.idade}`);
+// console.log(pessoal);
+
+function criarPessoa(nome, sobrenome, idade) {
+  return {
+    nome: nome,
+    sobrenome: sobrenome,
+    idade: idade,
+  };
+}
+
+pessoa1 = criarPessoa("Tuturito", "Silvana", 65);
+pessoa2 = criarPessoa("Joãozinho", "Ribeiro", 98);
+pessoa3 = criarPessoa("Passarinho", "Sodré", 23);
+
+console.log(pessoa1.nome, pessoa2.nome);
