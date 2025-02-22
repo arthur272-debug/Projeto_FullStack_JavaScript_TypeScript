7b52926d0378b77cbe443c17e1301fc62604c2b2
Arthur André <arthurdasilvaandre@gmail.com>
2025-02-22 14:10:32 -0300

    terminando_a_pratica

--- Exercicios_Basico/exercicio07/js/exercicio.js
+++ Exercicios_Basico/exercicio07/js/exercicio.js
@@ -1,6 +1,9 @@
 // Exercício sobre array, objeto e função
 function analisarDadosForm() {
   const form = document.querySelector(".form");
+  const resultado = document.querySelector(".resultado");
+
+  const pessoas = [];
 
   function receberEventoForm(event) {
     event.preventDefault();
@@ -8,10 +11,15 @@ function analisarDadosForm() {
     const sobrenome = form.querySelector(".sobrenome");
     const peso = form.querySelector(".peso");
     const altura = form.querySelector(".altura");
-
-    
+    pessoas.push({
+      nome: nome.value,
+      sobrenome: sobrenome.value,
+      peso: peso.value,
+      altura: altura.value,
+    });
+    resultado.innerHTML += `<p>${nome.value} ${sobrenome.value} ${peso.value} ${altura.value}</p>`;
+    console.log(pessoas);
   }
-
   form.addEventListener("submit", receberEventoForm);
 }
 analisarDadosForm();
