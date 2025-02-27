c89eddca37d313ebba0d1a679117246964825c7d
Arthur André <arthurdasilvaandre@gmail.com>
2025-02-26 22:42:56 -0300

    praticando_mais

---
+++ Aulas_Basico/aula18/script.js
@@ -0,0 +1,26 @@
+// Revisando os operadores de comparação
+
+console.log(10 == 10); // true
+console.log(10 == 5); // false
+console.log(5 == '5'); // true
+
+console.log(10 === 10); // true
+console.log(10 === 5); // false
+
+console.log(10 != 10); // false
+console.log(10 != 5); // true
+
+console.log(10 !== 10); // false
+console.log(10 !== 5); // true
+
+console.log(10 > 5); // true
+console.log(10 > 10); // false
+
+console.log(10 >= 10); // true
+console.log(5 >= 20); // false
+
+console.log(10 < 20); // true
+console.log(10 < 10); // false
+
+console.log(10 <= 10); // true
+console.log(10 <= 5); // false
