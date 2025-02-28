bbeeb3754a006706850e0c64cc729dd5590f3a61
Arthur André <arthurdasilvaandre@gmail.com>
2025-02-28 12:01:09 -0300

    praticando_expressoes

--- Aulas_Basico/aula19/script.js
+++ Aulas_Basico/aula19/script.js
@@ -1,2 +1,15 @@
 //Revisão sobre os operadores lógicos
 
+// const expressaoAnd = true && true && true && true; //true
+// const expressaoOr = false || false || false || true; //true
+// const expressaoNot = !true; //false
+
+// console.log(expressaoAnd);
+// console.log(expressaoOr);
+// console.log(expressaoNot);
+
+const usuario = "Arthur"; //form que o usuário digitou
+const senha = "123456";
+
+const usuarioLogar = usuario === "Arthur" && senha === "123456";
+console.log(usuarioLogar);
