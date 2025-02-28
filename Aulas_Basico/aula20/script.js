c3e30efc030d5e21e01240ba6ef5efb55abf8369
Arthur André <arthurdasilvaandre@gmail.com>
2025-02-28 14:19:53 -0300

    praticando_mais

--- Aulas_Basico/aula20/script.js
+++ Aulas_Basico/aula20/script.js
@@ -1,1 +1,17 @@
 // Aprendendo sobre curto-circuito
+
+function falaOi() {
+  return "Oi";
+}
+
+const vaiExecutar = "Tutu";
+let vaiExecutar2;
+
+console.log(vaiExecutar && falaOi());
+console.log(vaiExecutar2 && falaOi());
+
+// OR
+console.log(0 || null || "Fabio" || true);
+
+const corUsuario = null;
+const corPadrao = corUsuario || "preto";
