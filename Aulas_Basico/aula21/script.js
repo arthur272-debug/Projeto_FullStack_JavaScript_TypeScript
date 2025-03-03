7b25178d89689f3315fd7914a1840ace11bdcbe8
Arthur André <arthurdasilvaandre@gmail.com>
2025-03-03 11:18:34 -0300

    revisando_conceitos

--- Aulas_Basico/aula21/script.js
+++ Aulas_Basico/aula21/script.js
@@ -1,2 +1,21 @@
 //Revisando if, else if e else
 
+// const hora = 1;
+
+// if (hora >= 0 && hora < 12) {
+//   console.log("Bom dia!");
+// } else if (hora >= 12 && hora < 18) {
+//   console.log("Boa tarde!");
+// } else if (hora >= 18 && hora < 24) {
+//   console.log("Boa noite!");
+// } else {
+//   console.log("Horário inválido!");
+// }
+
+const vaiChover = false;
+
+if (vaiChover) {
+  console.log("Vou para a praia!");
+} else {
+  console.log("Ficarei em casa!");
+}
