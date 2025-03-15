cb893665395c29a705d76f97143065bb63d54eca
Arthur André <arthurdasilvaandre@gmail.com>
2025-03-14 21:15:22 -0300

    praticando_conceito

--- Aulas_Basico/aula22/index.js
+++ Aulas_Basico/aula22/index.js
@@ -1,2 +1,12 @@
 // Aprendendo sobre a operação ternária
 
+const pontuacaoUsuario = 999;
+const nivelUsuario =
+  pontuacaoUsuario >= 1000
+    ? "O status do usuário é VIP"
+    : "O status do usuário é Normal";
+
+const respostaUsuario = null;
+const respostaPadrao = respostaUsuario || "Não há resposta"; // outra forma de usar a operação ternária
+
+console.log(nivelUsuario, respostaPadrao);
