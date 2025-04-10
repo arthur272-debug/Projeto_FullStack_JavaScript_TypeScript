ee8aa30e201fc98c4f9b361d9e79f46d6e98d953
Arthur André <arthurdasilvaandre@gmail.com>
2025-04-10 17:49:52 -0300

    aprendendo_object_date

--- Aulas_Basico/aula23/index.js
+++ Aulas_Basico/aula23/index.js
@@ -1,1 +1,10 @@
 // Aprendendo sobre o objeto Date
+// rever e terminar de assistir a aula na udemy
+
+// const tres_horas = 60 * 60 * 3 * 1000; // 3 horas em milissegundos
+// const um_dia = 60 * 60 * 24 * 1000; // 1 dia em milissegundos
+// const data = new Date(0 + tres_horas - um_dia);
+// console.log(data.toString());
+
+const data = new Date(2025, 2, 30, 4, 0, 40, 0);
+console.log(data.toString());
