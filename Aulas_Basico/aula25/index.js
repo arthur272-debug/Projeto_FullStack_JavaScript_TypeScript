25d9985be71399ce755618d5bd44cafb7b17fa2f
Arthur André <arthurdasilvaandre@gmail.com>
2025-05-01 15:35:11 -0300

    entendendo_as_diferencas

--- Aulas_Basico/aula25/index.js
+++ Aulas_Basico/aula25/index.js
@@ -1,1 +1,31 @@
 // Aprendendo mais algumas diferenças entre let, var e const
+
+// const verdade = true;
+// let nome = "Ururu";
+// var nome2 = "Isabelon";
+
+// if (verdade) {
+//   let nome = "João";
+//   console.log(nome, nome2);
+//   var nome2 = "Maria";
+
+//   if (verdade) {
+//     var nome2 = "João";
+//     let nome = "Maria";
+//     console.log(nome, nome2);
+//   }
+// }
+// console.log(nome, nome2);
+
+// var cargo = "Gerente";
+
+// function exibeCargo() {
+//   console.log(cargo);
+// }
+// exibeCargo();
+
+console.log(cargo);
+// console.log(nome2); // ReferenceError: Cannot access 'nome2' before initialization
+
+var cargo = "Gerente";
+let cargo2 = "Gerente2";
