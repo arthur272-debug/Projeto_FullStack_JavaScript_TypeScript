bc9cd3362579b664d1ce523838823fe79f6a61ba
Arthur André <arthurdasilvaandre@gmail.com>
2025-05-02 12:30:32 -0300

    praticando_desestruturacao_via_objetos

--- Aulas_Basico/aula27/index.js
+++ Aulas_Basico/aula27/index.js
@@ -1,1 +1,21 @@
 // atribuição via desestruturação - objeto
+
+const carro = {
+  marca: "Fusca",
+  ano: 1968,
+  cor: "azul",
+  motor: {
+    cilindrada: 1.0,
+    tipo: "gasolina",
+  },
+};
+
+const { marca, ano = 1, cor, ...resto } = carro;
+const {
+  motor: { cilindrada },
+} = carro; // atribuição via desestruturação - objeto aninhado
+console.log(marca); // Fusca
+console.log(ano); // 1968
+console.log(carro.motor.tipo); // gasolina
+console.log(cilindrada); // 1.0
+console.log(resto); // { cor: 'azul', motor: { cilindrada: 1, tipo: 'gasolina' } }
