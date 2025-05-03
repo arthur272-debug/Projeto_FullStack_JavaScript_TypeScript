89faefaf8007b2476a4b2cf1ae895c5815648c11
Arthur André <arthurdasilvaandre@gmail.com>
2025-05-03 11:20:06 -0300

    revendo_for_classico

--- Aulas_Basico/aula28/index.js
+++ Aulas_Basico/aula28/index.js
@@ -1,1 +1,16 @@
-// revisando a estrutura de repetição for
+// revisando a estrutura de repetição for (clássico)
+
+// for (let i = 0; i < 2000; i++) {
+//   console.log(`Número ${i}`);
+// }
+
+// for (let i = 0; i <= 100; i++) {
+//   let numero_par = i % 2 === 0;
+//   console.log(numero_par ? `Número ${i} é par` : `Número ${i} é ímpar`);
+// }
+
+const avioes = ["airobus", "boeing", "embraer", "cessna", "airbus"];
+
+for (let i = 0; i < avioes.length; i++) {
+  console.log(`Avião ${i + 1}: ${avioes[i]}`);
+}
