3cfab1c27a32e2f912568ef2e1635398fa3d4798
Arthur André <arthurdasilvaandre@gmail.com>
2025-05-17 11:19:59 -0300

    praticando_conceito

--- Aulas_Basico/aula29/index.js
+++ Aulas_Basico/aula29/index.js
@@ -1,1 +1,17 @@
-// aprendendo sobre a estrutra de for in
+// aprendendo sobre o for in
+
+const frutas = ["maçã", "banana", "laranja"];
+
+const pessoas = {
+  nome: "Lucas",
+  idade: 25,
+  cidade: "São Paulo",
+};
+
+for (let chave in pessoas) {
+  console.log(chave, pessoas[chave]);
+}
+
+// for (let i in frutas) {
+//   console.log(i);
+// }
