2bc0dded24d40c5b994a30d9e4a33d627ee9a905
Arthur André <arthurdasilvaandre@gmail.com>
2025-07-05 11:05:10 -0300

    praticando_conceito

--- Aulas_Basico/aula30/index.js
+++ Aulas_Basico/aula30/index.js
@@ -1,1 +1,14 @@
 // aprendendo sobre o for of
+
+const nomes = ["João", "Maria", "Pedro", "Ana"];
+
+for (let nome of nomes) {
+  // acessa os valores do array
+  console.log(nome);
+}
+
+const pessoa = {
+  nome: "João",
+  idade: 30,
+  cidade: "São Paulo",
+};
