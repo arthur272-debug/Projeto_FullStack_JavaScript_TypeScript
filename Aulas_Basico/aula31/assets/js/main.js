7b6cb64ed0b71f8c55d67fd9e01d4055bfd5e43a
Arthur André <arthurdasilvaandre@gmail.com>
2025-07-25 17:35:36 -0300

    praticando_NodeList

--- Aulas_Basico/aula31/assets/js/main.js
+++ Aulas_Basico/aula31/assets/js/main.js
@@ -1,2 +1,11 @@
 // Exercício com NodeList
-const paragrafos = document.querySelectorAll("p");
+const paragrafos = document.querySelector(".paragrafos");
+const ps = paragrafos.querySelectorAll("p");
+
+const estilosBody = getComputedStyle(document.body);
+const backgroundColorBody = estilosBody.backgroundColor;
+
+for (let p of ps) {
+  p.style.backgroundColor = backgroundColorBody;
+  p.style.color = "#FFF";
+}
