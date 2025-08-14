21f1dcbe164ebcd1a772b03bec1d58fd49cceb66
Arthur André <arthurdasilvaandre@gmail.com>
2025-08-14 09:25:36 -0300

    praticando os conceitos

--- Aulas_Basico/aula32/script.js
+++ Aulas_Basico/aula32/script.js
@@ -1,1 +1,34 @@
 // aprendendo sobre while e do while
+
+// let i = 0;
+
+// while (i <= 10) {
+//   console.log(i);
+//   i++;
+// }
+
+// console.log("------------------");
+
+// i = 0;
+// const nome = "Tutu";
+
+// while (i < nome.length) {
+//   console.log(nome[i]);
+//   i++;
+// }
+
+function random(min, max) {
+  const numero = Math.floor(Math.random() * (max - min + 1)) + min;
+  return numero;
+}
+
+const min = 1;
+const max = 50;
+let aleatorio = random(min, max);
+
+do {
+  console.log(aleatorio);
+  aleatorio = random(min, max);
+} while (aleatorio !== 25);
+
+console.log("O número 25 foi encontrado!");
