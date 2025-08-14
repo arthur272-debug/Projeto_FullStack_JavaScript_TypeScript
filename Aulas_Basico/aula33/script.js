2afc53a12c2a5d5ef0973575aab2dfbe7f7528d4
Arthur André <arthurdasilvaandre@gmail.com>
2025-08-14 11:03:01 -0300

    praticando_conceitos_aprendidos

--- Aulas_Basico/aula33/script.js
+++ Aulas_Basico/aula33/script.js
@@ -1,1 +1,31 @@
 // aprendendo sobre o Break e Continue
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (let numero of numeros) {
+  if (numero === 2) {
+    console.log("Número 2 pulado");
+    continue;
+  }
+  console.log(numero);
+
+  if (numero === 9) {
+    console.log("9 encontrado, saindo...");
+    break;
+  }
+}
+console.log("Fim do loop");
+
+while (true) {
+  let numero = Math.floor(Math.random() * 10) + 1;
+  console.log("Número aleatório:", numero);
+  if (numero === 2) {
+    console.log("Número 2 pulado");
+    numero = Math.floor(Math.random() * 10) + 1;
+    continue;
+  }
+
+  if (numero === 5) {
+    console.log("5 encontrado, saindo...");
+    break;
+  }
+}
