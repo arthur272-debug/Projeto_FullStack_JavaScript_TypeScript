90620e96cd4dda47e9e4d6c23772ea44e2888993
Arthur André <arthurdasilvaandre@gmail.com>
2025-08-20 21:34:01 -0300

    formatando_arquivo

---
+++ Exercicios_Basico/exercicio12/script.js
@@ -0,0 +1,4 @@
+// Exercício de lógica de programação
+// -> Escreva uma função chamada ePaisagem que recebe dois argumentos:
+// largura e altura de uma imagem(number).
+// Retorne true se a imagem estiver no modo paisagem
