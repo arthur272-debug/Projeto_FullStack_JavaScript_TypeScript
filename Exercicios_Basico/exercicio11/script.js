68432c5d1e8368370df8f638e774dd9ae52a9765
Arthur André <arthurdasilvaandre@gmail.com>
2025-08-20 21:31:22 -0300

    realizando_exercicio

--- Exercicios_Basico/exercicio11/script.js
+++ Exercicios_Basico/exercicio11/script.js
@@ -1,1 +1,14 @@
 // exercício sobre lógica de programação
+// -> Escreva uma função que receba 2 números e retorne o maior deles
+
+function maiorNumero(num1, num2) {
+  return num1 > num2 ? num1 : num2;
+}
+
+const maiorNumero2 = (num1, num2) => (num1 > num2 ? num1 : num2); // arrow function
+
+console.log(maiorNumero(5, 10)); // 10
+console.log(maiorNumero(20, 15)); // 20
+
+console.log(maiorNumero2(50, 10)); // 50
+console.log(maiorNumero2(1, 15)); // 15
