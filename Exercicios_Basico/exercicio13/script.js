faf3ecb7fa1450f269d8d4eff6eac14e61006531
Arthur André <arthurdasilvaandre@gmail.com>
2025-08-21 21:40:26 -0300

    formatando_arquivo

---
+++ Exercicios_Basico/exercicio13/script.js
@@ -0,0 +1,8 @@
+// Exercício sobre lógica de programação -> Praticando melhor
+// Escreva uma função que recebe um número e retorna o seguinte:
+// Número é divisível por 3 = Fizz
+// Número é divisível por 5 = Buzz
+// Número é divisível por 3 e 5 = FizzBuzz
+// Número NÃO é divisível por 3 e 5 = Retorna o próprio número
+// Chegar se o número é realmente um número
+// Use a função com números de 0 a 100
