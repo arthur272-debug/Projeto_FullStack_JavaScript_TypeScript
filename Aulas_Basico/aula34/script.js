c2cfca9005d64b4a51406d12ceb168a4c2b05d8d
Arthur André <arthurdasilvaandre@gmail.com>
2026-01-17 19:09:25 -0300

    reaprendendo_tratamento_erro1

--- Aulas_Basico/aula34/script.js
+++ Aulas_Basico/aula34/script.js
@@ -1,1 +1,22 @@
 // Revisando sobre o tratamento e lançamento de erros(try,catch,throw)
+
+// try {
+//   console.log(ab);
+// } catch (erro) {
+//   console.log("Variável não declarada");
+//   console.log(erro);
+// }
+
+function somarNumeros(x, y) {
+  if (typeof x !== "number" || typeof y !== "number") {
+    throw new ReferenceError("Ambos os parâmetros devem ser números.");
+  }
+  return x + y;
+}
+
+try {
+  console.log(somarNumeros(2, 3));
+  console.log(somarNumeros("a", 60));
+} catch (error) {
+  console.log(error);
+}
