28aae8aa0d5fc11e9cea47196934cebae65f18dc
Arthur André <arthurdasilvaandre@gmail.com>
2026-09-11 11:20:03 -0300

    aprender_retorno_funcoes

--- Funcoes_Avancado/aula39/script.js
+++ Funcoes_Avancado/aula39/script.js
@@ -1,1 +1,27 @@
 // Aprendendo sobre retorno de funções
+// function falaFrase(comeco) {
+//   function falaRestoFrase(resto) {
+//     return comeco + ", " + resto;
+//   }
+//   return falaRestoFrase;
+// }
+
+// const comeco = falaFrase("Bom dia");
+// const fim = comeco("Senhoras e Senhores!");
+// console.log(fim);
+
+function definirMultiplicador(multiplicador) {
+  return function (numero) {
+    return numero * multiplicador;
+  };
+}
+
+const duplica = definirMultiplicador(2);
+const triplica = definirMultiplicador(3);
+const quadriplica = definirMultiplicador(4);
+const quintuplifica = definirMultiplicador(5);
+
+console.log(duplica(10));
+console.log(triplica(10));
+console.log(quadriplica(10));
+console.log(quintuplifica(10));
