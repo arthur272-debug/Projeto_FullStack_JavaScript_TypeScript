818836d4db43b55d4c9cebc19710f5bdd8bf1f75
Arthur André <arthurdasilvaandre@gmail.com>
2026-09-12 19:00:06 -0300

    praticando_closures

--- Funcoes_Avancado/aula41/script.js
+++ Funcoes_Avancado/aula41/script.js
@@ -1,1 +1,16 @@
 // Revisando sobre closures
+
+function multiplicarFator(n) {
+  return function () {
+    return n * n;
+  };
+}
+
+funcao = multiplicarFator(2);
+funcao2 = multiplicarFator(3);
+
+console.dir(funcao);
+console.dir(funcao2);
+
+console.log(funcao());
+console.log(funcao2());
