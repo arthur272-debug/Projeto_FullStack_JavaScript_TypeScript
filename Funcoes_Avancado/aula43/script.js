5264b2172153e556970037c42d6ffd0acd9432d4
Arthur André <arthurdasilvaandre@gmail.com>
2026-09-19 21:22:56 -0300

    aprender_funcoes_imediatas

--- Funcoes_Avancado/aula43/script.js
+++ Funcoes_Avancado/aula43/script.js
@@ -1,1 +1,12 @@
 //Aprendendo sobre as funções imediatas - IIFE (Immediately Invoked Function Expression)
+(function fazerSaudacao() {
+  const nome = "John";
+  console.log("Olá, meu nome é " + nome);
+})(30, 20, 40);
+
+function fazerSaudacao() {
+  const nome = "Paul";
+  console.log("Olá, meu nome é " + nome);
+}
+
+fazerSaudacao();
